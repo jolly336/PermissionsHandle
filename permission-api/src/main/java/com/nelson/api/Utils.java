b68aa58b4f17ddc557d81ec29dba4826243cca3f
wanghaoxun <wanghaoxun@guazi.com>
2017-05-05 19:37:50 +0800

    add Permission handle factory

--- permission-api/src/main/java/com/nelson/api/Utils.java
+++ permission-api/src/main/java/com/nelson/api/Utils.java
@@ -1,6 +1,13 @@
 package com.nelson.api;
 
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Nelson on 17/5/5.
@@ -8,13 +15,30 @@ import android.os.Build;
 
 public final class Utils {
 
-    private static boolean overMarshmallow;
-
     private Utils() {
     }
 
-
     public static boolean isOverMarshmallow() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public static List<String> findDeniedPermissions(Activity activity, String... permission) {
+        List<String> denyPermissions = new ArrayList<>();
+        for (String value : permission) {
+            if (activity.checkSelfPermission(value) != PackageManager.PERMISSION_GRANTED) {
+                denyPermissions.add(value);
+            }
+        }
+        return denyPermissions;
+    }
+
+    public static Activity getActivity(Object object) {
+        if (object instanceof Fragment) {
+            return ((Fragment) object).getActivity();
+        } else if (object instanceof Activity) {
+            return (Activity) object;
+        }
+        return null;
     }
 }
