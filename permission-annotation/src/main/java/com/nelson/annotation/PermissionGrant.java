2c6cd3fedc06158abd9afe9ff8aacc39941c277b
wanghaoxun <wanghaoxun@guazi.com>
2017-05-05 19:18:52 +0800

    add annotation java module

---
+++ permission-annotation/src/main/java/com/nelson/annotation/PermissionGrant.java
@@ -0,0 +1,20 @@
+package com.nelson.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Grant permission successfully.
+ * Created by Nelson on 17/5/5.
+ */
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.METHOD)
+@Documented
+public @interface PermissionGrant {
+
+    int value();
+}
---
+++ permission-annotation/src/main/java/com/nelson/annotation/ShowRequestPermissionRationale.java
@@ -0,0 +1,20 @@
+package com.nelson.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Should show request permission rationale.
+ * Created by Nelson on 17/5/5.
+ */
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.METHOD)
+@Documented
+public @interface ShowRequestPermissionRationale {
+
+    int value();
+}
